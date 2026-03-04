0eb77591dc07a255a9744b58fdff2d466f5873ef
Joseph <marinmaor97@gmail.com>
2026-03-03 19:51:08 -0500

    feat: implement country search, filtering, and detailed modal viwe

--- src/main.ts
+++ src/main.ts
@@ -16,27 +16,30 @@ interface Country {
 //Aqui guardamos la verda de la aplicacion en todo momento
 let allCountries: Country[] = []; //Arreglo de todos los paises
 let countries: Country[] = []; //Arreglo de los paises que se muestran en pantalla
-let favoriteCodes: string[] = JSON.parse(localStorage.getItem("favs") || "[]"); //Arreglo de los paises favoritos
+let favoriteCodes: string[] = JSON.parse(localStorage.getItem("favs") || "[]"); //Arreglo de los paises favoritos json.parse es para convertir el string en un arreglo
 let isLoading: boolean = false; //Bandera para saber si estamos cargando los paises
+let searchTimer: number; //El cronometro de la busqueda
 
 // 3. DOM ELEMENTS
 //Aqui guardamos los elementos del DOM('HTML)
-const resultContainer =
+const resultsContainer =
   document.querySelector<HTMLElement>("#result-container");
-const inputSearch = document.querySelector<HTMLInputElement>("#input-search");
+const inputSearch = document.querySelector<HTMLInputElement>("#search-input");
 const themeToggle = document.querySelector<HTMLButtonElement>("#theme-toggle");
-//boton favoritos
-//modal
+const modalContainer =
+  document.querySelector<HTMLDivElement>("#modal-container");
+//const modalClose = document.querySelector<HTMLButtonElement>("#modal-close");
+const modalContent = document.querySelector<HTMLDivElement>("#modal-content");
 
 // 4. Core funtion
 //Aqui va la funcion de pintar en el DOM
 
 const render = (paises = countries) => {
   // 1. SEGURIDAD: sino existe el contenedor, no hacemos nada "ASI NO SE ROMPE EL CODIGO"
-  if (!resultContainer) return;
+  if (!resultsContainer) return;
 
   // 2. Limpieza: Antes de pintar limpiamos el contenedor
-  resultContainer.innerHTML = "";
+  resultsContainer.innerHTML = "";
 
   // 3. EL BUCLE (MAP): Vamos a transformar el array de objetos en un String de (HTML)
   // 3.1 EL METODO MAP: Recorre el arreglo y devuelve un nuevo arreglo con los elementos transformados
@@ -62,7 +65,7 @@ const render = (paises = countries) => {
 
   // 4. INYECCIÓN: Pegamos todas las cards en el contenedor del HTML
   //4.1 EL METODO JOIN: Convierte un array en un String
-  resultContainer.innerHTML = htmlCards.join("");
+  resultsContainer.innerHTML = htmlCards.join("");
 };
 
 //Funcion de pedir los datos a la Api
@@ -102,8 +105,8 @@ const fetchCountries = async () => {
   } catch (error) {
     // 7. MANEJO DE ERRORES: Mostramos un mensaje de error en la consola
     console.error("Error detectado:", error);
-    if (resultContainer) {
-      resultContainer.innerHTML = `
+    if (resultsContainer) {
+      resultsContainer.innerHTML = `
     <p class="col-span-full text-center text-red-500 font-bold">Error: ${(error as Error).message}</p>`;
     }
   } finally {
@@ -113,14 +116,66 @@ const fetchCountries = async () => {
 };
 
 //Funcion para la busqueda (con el debounce)
-const handleSearch = () => {
-  //aqui ira el cronometro (setTimeout)
-  //filter()
+const handleSearch = (e: Event) => {
+  const query = (e.target as HTMLInputElement).value.toLocaleLowerCase().trim();
+
+  // 1.DEBOUNCE: Si el usuario escribe rapido, borramos el cronometro anterio
+  clearTimeout(searchTimer);
+
+  // 2. Iniciamos un nuevo cronometro
+  searchTimer = setTimeout(() => {
+    console.log("Buscando", query);
+
+    // 3. FILTRADO: Buscanmos en allcountries ("Nuestra base de datos limpia")
+    countries = allCountries.filter(
+      (country) =>
+        country.name.toLocaleLowerCase().includes(query) ||
+        country.region.toLowerCase().includes(query),
+    );
+
+    // 4. ACTUALIZACIÓN: Actualizamos la interfaz
+    render();
+
+    // 5. FEEDBACK: Si no hay nada avisamos al usuario
+    if (countries.length === 0 && resultsContainer) {
+      resultsContainer.innerHTML = `
+    <div class="col-span-full text-center py-2">
+    <p class="text-4-xl mb-4 >🎍</p>
+    <h3 class="text-xl font-bold opacity-50">No countries found for "${query}"</h3>
+    </div>
+    `;
+    }
+  }, 500);
 };
 
 //Funcion para el modal
-const openModal = (id: string) => {
-  //aqui buscaremos el pais por su id
+const openModal = (code: string) => {
+  const country = allCountries.find((c) => c.cca3 === code);
+  // si no lo encontramos, no hacemos nada
+  if (!country || !modalContainer || !modalContent) return;
+
+  // 1. Mostramos el modal quitando la clase hidden
+  modalContainer.classList.remove("hidden");
+
+  // 2. Limpiamos el contenido del modal
+  modalContent.innerHTML = `
+<button id="close-modal" class="absolute top-5 text-2xl hover:rotate-90 transition-transform cursor-pointer">❌</button>
+<div class="p-8">
+  <img src="${country.flag}" class="w-full h-64 objetc-cover rounded-2xl mb-6 shadow-lg" />
+  <h2 class="text-3xl font-black mb-4 dark:text-white">${country.name}</h2>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-6 text-slate-600 dark:text-slate-400">
+    <p><span class="font-bold text-slate-900 dark:text-slate-200">Region:</span> ${country.region}</p>
+    <p><span class="font-bold text-slate-900 dark:text-slate-200">Capital:</span> ${country.capital}</p>
+    <p><span class="font-bold text-slate-900 dark:text-slate-200">Population:</span> ${country.population.toLocaleString()}</p>
+    <p><span class="font-bold text-slate-900 dark:text-slate-200">Official Code:</span> ${country.cca3}</p>
+  </div>
+</div>
+`;
+
+  // 3. Agregamos el evento para cerrar el modal
+  document.getElementById("close-modal")?.addEventListener("click", () => {
+    modalContainer.classList.add("hidden");
+  });
 };
 
 //FUNCIÓN PAR CAMBIAR EL TEMA (DARK/LIGHT "theme-toggle")
@@ -170,13 +225,71 @@ const filterByRegion = (region: string) => {
   render();
 };
 
+//Funcion para el boton de favoritos
+const toggleFavorites = (code: string) => {
+  // 1. Buscamos el pais por su cca3
+  const index = favoriteCodes.indexOf(code); // indexof devuelve -1 si no lo encuentra
+
+  // 2. si no lo encontramos, (index -1), lo agregamos
+  if (index === -1) {
+    favoriteCodes.push(code);
+    console.log("❤️ agregado: ${code}");
+
+    // 3. si lo encontramos, lo quitamos
+  } else {
+    favoriteCodes.splice(index, 1); // splice es para quitar un elemento del arreglo
+    console.log("❤️ Quitado: ${code}");
+  }
+
+  // 4. PERSISTENCIA: Guardamos los favoritos en el baul del navegador (localstorage)
+  localStorage.setItem("favs", JSON.stringify(favoriteCodes));
+
+  // 5. ACTUALIZACIÓN DE DATOS: Recoremostodos los paises y le cambiamos el switch isFavorite
+  allCountries = allCountries.map((country) => {
+    if (country.cca3 === code) {
+      return { ...country, isFavorite: !country.isFavorite }; //creamos una copia del pais y le cambiamos el switch isFavorite
+    }
+    return country; //si no lo encontramos, lo devolvemos tal cual
+  });
+
+  // 6. SINCRONIZACIÓN DE VISTA: Hacemos lo misnmo con la lista que se esta mostrando actualmente, por si hay filtros
+  countries = countries.map((c) => {
+    if (c.cca3 === code) return { ...c, isFavorite: !c.isFavorite }; //creamos una copia del pais y le cambiamos el switch isFavorite
+    return c;
+  });
+
+  // 5. PINTAR: Actualizamos la interfaz para que se vean los cambios
+  render();
+};
+
 // 5. even listeners
 //Aqui escuchamos los eventos del DOM
 inputSearch?.addEventListener("input", handleSearch);
 
 //favitoris
+resultsContainer?.addEventListener("click", (e) => {
+  // .closest() busca hacia arriba si hicimos click en un boton de favoritos
+  const btn = (e.target as HTMLElement).closest(".btn-fav"); //
+  if (btn) {
+    // obtenemos el id que guardamos en el atributo data-id
+    const id = btn.getAttribute("data-id");
+    if (id) toggleFavorites(id);
+  }
 
-//modal
+  //modal
+  const target = e.target as HTMLElement; //obtenemos el elemento que hicimos click
+  // 1. Si cliclamos en el botono de favoritos, ya lo manejamos antes con stoPropagation()
+  if (target.closest(".btn-fav")) return;
+
+  // 2. si clicamos en cualquier otra parte de la tarjeta
+  const card = target.closest(".country-card");
+  if (card) {
+    const countryCode = card.getAttribute("data-id");
+    if (countryCode) {
+      openModal(countryCode);
+    }
+  }
+});
 
 //theme toggle(dark/light "toggle-theme")
 themeToggle?.addEventListener("click", toggleTheme);
